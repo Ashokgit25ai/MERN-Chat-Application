79ac5aa9a3eca155ce716e643f2eca747b05b53a
Ashokgit25ai <anithaashok944@gmail.com>
2026-07-28 20:10:00 +0530

    add signup page and styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+
 
 const App = () => {
   return (
     <div>
-      Hi, This is my react App
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
