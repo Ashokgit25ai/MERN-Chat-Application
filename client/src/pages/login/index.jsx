b0f8e15e728003c83c165fe23163ecca99f4005f
Ashokgit25ai <anithaashok944@gmail.com>
2026-07-29 11:47:43 +0530

    feat: implement signup authentication flow

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const index = () => {
+  const [user, setUser] = useState({
+    email:'',
+    password:''
+  });
+
+  const onLoginFormSubmit = (event) =>{
+    event.preventDefault();
+    console.log(user);
+  };
+
   return (
-    <div>
-      Login
+    <div className='container'>
+      <div className="container-back-img"></div>
+      <div className="container-back-color"></div>
+      <div className="card">
+        <div className="card-title">
+          <h1>Login Here</h1>
+        </div>
+        <div className="form" onSubmit={onLoginFormSubmit}>
+          <form>
+            <input type="email" placeholder='Email' value={user.email} 
+              onChange={(e) => setUser({...user, email: e.target.value})} />
+            <input type="password" placeholder='password' value={user.password} 
+              onChange={(e) => setUser({...user, password: e.target.value})} />
+            <button>Login</button>
+          </form>
+          <div className="card-terms">
+            <span>Don't have an account yet?
+               <Link to="/signup">Sign Up</Link>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
