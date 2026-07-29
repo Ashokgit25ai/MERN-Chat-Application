0b8f8d6b4807a47a7fc9b435b0c8ed14ac90a470
Ashokgit25ai <anithaashok944@gmail.com>
2026-07-29 23:08:47 +0530

    implement function to get current user details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import { Toaster } from 'react-hot-toast';
+import ProtectedRoute from './components/ProtectedRoute';
 
 
 const App = () => {
@@ -13,7 +14,11 @@ const App = () => {
       <Toaster position="top-center" reverseOrder={true} />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          <Route path='/' element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>}>
+          </Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/signup' element={<Signup />}></Route>
         </Routes>
