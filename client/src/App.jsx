cbe20cd8187bece038ecfaa9c1a294fd9227763e
Ashokgit25ai <anithaashok944@gmail.com>
2026-07-29 13:43:14 +0530

    feat: add toast notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,14 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
+import { Toaster } from 'react-hot-toast';
 
 
 const App = () => {
   return (
+    
     <div>
+      <Toaster position="top-center" reverseOrder={true} />
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />}></Route>
