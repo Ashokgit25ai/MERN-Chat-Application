a56f089d7d2536b3acc701e6989afbd083c66af9
Ashokgit25ai <anithaashok944@gmail.com>
2026-07-31 17:04:52 +0530

    feat: add current chat filtering and user search

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getLoggedUser, getAllUsers } from "../pages/apiCalls/users";
+import { getAllChats } from "../pages/apiCalls/chat";
 import { useDispatch, useSelector } from "react-redux";
 import { hideLoader, showLoader } from "../redux/loaderSlice";
 import toast from "react-hot-toast";
-import { setUser, setAllUsers } from "../redux/userSlice";
+import { setUser, setAllUsers, setAllCurrentChats } from "../redux/userSlice";
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useSelector(state => state.userReducer);
@@ -54,12 +55,32 @@ const ProtectedRoute = ({ children }) => {
     }
   };
   
+  //GET ALL CHATS CONNECTED WITH CURRENT USER
+  const getAllCurrentUserChats = async () => {
+    let response = null;
+    try{
+      dispatch(showLoader());
+      response = await getAllChats();
+      dispatch(hideLoader()); 
+
+      if(response.success){
+        dispatch(setAllCurrentChats(response.data));
+      }else{
+        toast.error(response.message);
+        window.location.href = '/login'
+      }
+    }catch(error){
+      dispatch(hideLoader());
+      navigate('/login')
+    }
+  }
 
   useEffect(() => {
     if (localStorage.getItem("token")) {
       //write a logic to get the current user details
       getLoggedInUser();
       getAllLoggedUsers();
+      getAllCurrentUserChats();
     } else {
       navigate("/login");
     }
--- client/src/pages/home/components/UserList.jsx
+++ client/src/pages/home/components/UserList.jsx
@@ -1,34 +1,48 @@
-import React from 'react'
-import '../styles/userList.css'
-import { useSelector } from 'react-redux';
+import React from "react";
+import "../styles/userList.css";
+import { useSelector } from "react-redux";
 
-const UserList = ({searchKey}) => {
-    const { allUsers } = useSelector(state => state.userReducer)
-  return (
-        allUsers
-        .filter(user => {
-            return searchKey && (`${user.firstname} ${user.lastname}`.toLowerCase().includes(searchKey.toLowerCase()));
-        })
-        .map(user => (
-                <div className="user-search-filter">
-                    {user.profilePic && <div className="user-profile-pic">
-                        <img src={user.profilePic} alt="Profile Pic" className='user-profile-image' key={user._id}/>
-                        
-                    </div>}
-                    {!user.profilePic && <div className="user-profile-pic">
-                        {user.firstname.charAt(0).toUpperCase() + user.lastname.charAt(0).toUpperCase()}
-                    </div>}
-                    <div className="filter-user-details">
-                        <div className="user-display-name">{`${user.firstname} ${user.lastname}`}</div>
-                        <div className="user-display-email">{user.email}</div>
-                    </div>
-                    <div className="user-start-chat">
-                        <button className="user-start-chat-btn">Start Chat</button>
-                    </div>
-                </div>
-            )
-        )
-    )
+const UserList = ({ searchKey }) => {
+  const { allUsers } = useSelector((state) => state.userReducer);
+  const { allCurrentChats } = useSelector((state) => state.userReducer);
+  return allUsers
+    .filter((user) => {
+      return (
+        searchKey ?
+        `${user.firstname} ${user.lastname}`
+          .toLowerCase()
+          .includes(searchKey.toLowerCase()) :
+          allCurrentChats.some(chat => chat.members.includes(user._id))
+      );
+    })
+    .map((user) => (
+      <div className="user-search-filter" key={user._id}>
+        {user.profilePic && (
+          <div className="user-profile-pic">
+            <img
+              src={user.profilePic}
+              alt="Profile Pic"
+              className="user-profile-image"
+            />
+          </div>
+        )}
+        {!user.profilePic && (
+          <div className="user-profile-pic">
+            {user.firstname.charAt(0).toUpperCase() +
+              user.lastname.charAt(0).toUpperCase()}
+          </div>
+        )}
+        <div className="filter-user-details">
+          <div className="user-display-name">{`${user.firstname} ${user.lastname}`}</div>
+          <div className="user-display-email">{user.email}</div>
+        </div>
+        {!allCurrentChats.find((chat) => chat.members.includes(user._id)) && (
+          <div className="user-start-chat">
+            <button className="user-start-chat-btn">Start Chat</button>
+          </div>
+        )}
+      </div>
+    ));
 };
 
 export default UserList;
